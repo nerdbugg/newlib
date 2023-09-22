fa7859a40504d3bcb83bea48454a8edd24fdd793
chuyukang <17082643+Chuyukang@users.noreply.github.com>
2023-09-22 16:01:23 +0800

    carrier: implementaion of syscalls and crt0

---
+++ newlib/libc/sys/carrier/crt0.c
@@ -0,0 +1,20 @@
+__asm__(
+".section .sdata,\"aw\"\n"
+".text\n"
+".global _start\n"
+".type _start, %function\n"
+".weak __global_pointer$\n"
+".hidden __global_pointer$\n"
+".option push\n"
+".option norelax\n\t"
+"lla gp, __global_pointer$\n"
+".option pop\n\t"
+"mv a0, sp\n"
+".weak _DYNAMIC\n"
+".hidden _DYNAMIC\n\t"
+"lla a1, _DYNAMIC\n\t"
+"andi sp, sp, -16\n\t"
+"call main\n\t"
+"tail _exit"
+);
+
